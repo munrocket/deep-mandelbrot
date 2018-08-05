98f045e0a46ebbf5092032af1e294d4bdb842b5e
munrocket <munrocket@protonmail.com>
2018-08-05 19:32:14 +0400

    optimization & second color sheme

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # mandelbrot-set
-Tiny version of famous fractal in [web page](http://htmlpreview.github.io/?https://github.com/munrocket/mandelbrot-set/blob/master/main.html)
+Small version of famous fractal in [web page](http://htmlpreview.github.io/?https://github.com/munrocket/mandelbrot-set/blob/master/main.html)
 
 ![](https://i.imgur.com/6Nr1KXJ.png)
+
+[X] Zooming
+[X] Optimization with only 3 multiplication in cycle
+[X] 2 color sheme
--- mandelbrot.js
+++ mandelbrot.js
@@ -3,46 +3,60 @@ let savedPos = null;
 let canvas = document.getElementById("canvas");
 let target = { Re: -0.5, Im: 0, dRe: 1.5, dIm: 1 };
 
-async function mandelbrot(imageData, width, height, target) {
+function mandelbrot(imageData, width, height, target) {  
   let iterations = document.getElementById("iterations").value;
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < height; y++) {
 
-      //calcuation
-      let i = 0, zRe = 0, zIm = 0;
+      let i = 0, zRe = 0, zIm = 0, zReSqr = 0, zImSqr = 0;
       let cRe = target.Re - target.dRe + 2 * target.dRe * x / width;
       let cIm = target.Im + target.dIm - 2 * target.dIm * y / height;
-      while (zRe * zRe + zIm * zIm < 4 && i < iterations) {
+      while (zReSqr + zImSqr < 4 && i < iterations) {
         i++;
-        let zRe0 = zRe;
-        zRe = zRe * zRe - zIm * zIm;
-        zIm = 2 * zRe0 * zIm;
-        zRe += cRe;
+        zIm = (zRe + zIm) * (zRe + zIm) - zReSqr - zImSqr;
+        zRe = zReSqr - zImSqr;
         zIm += cIm;
+        zRe += cRe;
+        zImSqr = zIm * zIm;
+        zReSqr = zRe * zRe;
       }
 
-      //color
-      let abs = Math.abs;
-      let t = (10 * i / 256) % 6;
       let index = (y * width + x) * 4;
+      imageData.data[index + 3] = 255;
+      colorize(index, i, iterations, imageData)
+    }
+  }
+}
+
+function colorize(index, i, iterations, imageData) {
+  let sheme = document.getElementById("colorizing").selectedIndex;
+  let colorStep = document.getElementById("colorStep").value;
+  switch (sheme) {
+    case 1:
       if (i == iterations) {
         imageData.data[index] = 0; imageData.data[index + 1] = 0; imageData.data[index + 2] = 0;
       } else {
+        let abs = Math.abs;
+        let t = (colorStep * i / 256) % 6;
         imageData.data[index] = (2 - abs(t-5) + abs(t-4) + abs(t-2) - abs(t-1)) * 127.5;
         imageData.data[index + 1] = (abs(t-4) - abs(t-3) - abs(t-1) + abs(t)) * 127.5;
         imageData.data[index + 2] = (abs(t-6) - abs(t-5) - abs(t-3) + abs(t-2)) * 127.5;
       }
-      imageData.data[index + 3] = 255;
-    }
+      break;
+    default:
+      imageData.data[index] = 255 * (iterations - i) / iterations ;
+      imageData.data[index + 1] = imageData.data[index];
+      imageData.data[index + 2] = imageData.data[index];
+      break;
   }
-  setTimeout(clearInterval(looping), 500);
 }
 
 function draw(mandelbrot, target) {
   let context = canvas.getContext("2d");
   let imageData = context.createImageData(canvas.width, canvas.height);
   mandelbrot(imageData, canvas.width, canvas.height, Object.assign({}, target));
-  setTimeout(context.putImageData(imageData, 0, 0), 200);
+  context.putImageData(imageData, 0, 0);
+  document.getElementById("currentPos").innerHTML = `(${target.Re}, ${target.Im}) with ${Math.floor(1.5 / target.dRe)}x zoom`;
 }
 
 function getMousePos(e) {
